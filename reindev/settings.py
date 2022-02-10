f431ea56d92018ae68f6184cc1f2f573d2a4be24
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-02-09 22:04:42 -0500

    nuevo deploy

--- reindev/settings.py
+++ reindev/settings.py
@@ -131,9 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 
@@ -141,9 +139,16 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 LOGIN_REDIRECT_URL = 'home'
 
-STATICFILES_DIRS =[
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+
      os.path.join(BASE_DIR,"static"),
- ]
+)
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
