de2a629e5efaa9ecebb37c7ff4b45515f3c41ece
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-02-12 19:25:53 -0500

    se creo la vista de articulo de completo y se agregaron los item para edit y delete 5

--- reindev/urls.py
+++ reindev/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     
     
        
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # https://www.youtube.com/watch?v=srOLTZYxMTc
