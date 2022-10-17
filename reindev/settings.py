8d352a2d012543c0577cded3f94e88e754bce93b
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-10-17 14:32:04 -0500

    Version 17102022_2 BD y Archivos estaticos error archivos estaticos

--- reindev/settings.py
+++ reindev/settings.py
@@ -177,7 +177,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 STATICFILES_DIRS = (
