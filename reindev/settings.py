0c1b136baf832766090c73b9ccb124f315b08eef
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-10-17 14:46:06 -0500

    Version 17102022_2 BD y Archivos estaticos error de acceso 500

--- reindev/settings.py
+++ reindev/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*csxruv2j-9f9j&tgy*vq)jn=0of-1b2qda5w1_4g)96pf3-$t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['165.227.90.164']
 # ALLOWED_HOSTS =[]
@@ -99,24 +99,24 @@ WSGI_APPLICATION = 'reindev.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
+    'default':{
+        'ENGINE':'django.db.backends.postgresql_psycopg2',
+        'NAME':'rein',
+        'USER':'rein_admin',
+        'PASSWORD':'rein1234',
+        'HOST':'localhost',
+        'POST':'',
     }
-else:
-    DATABASES = {
-        'default':{
-            'ENGINE':'django.db.backends.postgresql_psycopg2',
-            'NAME':'rein',
-            'USER':'rein_admin',
-            'PASSWORD':'rein1234',
-            'HOST':'localhost',
-            'POST':'',
-        }
-    }
+}
 
 # import dj_database_url 
 # from decouple import config
