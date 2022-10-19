27e6568daa148270a7e0b53cfcd798b0585a9f1b
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-10-19 00:24:54 -0500

    configuracion del host 4

--- reindev/settings.py
+++ reindev/settings.py
@@ -179,8 +179,8 @@ MEDIA_URL = '/media/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR,"staticfiles")]
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
+MEDIA_ROOT=os.path.join(BASE_DIR,'/media/')
 
 
 
