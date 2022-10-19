a410b2aaff898437c3d693e24fe56ecea3a9b037
Ing. Alexander Varela <jkaalexkei@gmail.com>
2022-10-19 00:16:32 -0500

    configuracion del host 3

--- reindev/settings.py
+++ reindev/settings.py
@@ -178,13 +178,11 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,"staticfiles")]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-STATICFILES_DIRS = (
 
-     os.path.join(BASE_DIR,"static"),
-)
 
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
